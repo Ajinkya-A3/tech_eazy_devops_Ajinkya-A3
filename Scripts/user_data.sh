7c5e502c7ed1f6aeb26be2b3cd4de79bb6237b39
Ajinkya-A3 <ajinkyaa3xd@gmail.com>
2025-07-25 21:26:39 +0530

    changed the bash script

--- Scripts/user_data.sh
+++ Scripts/user_data.sh
@@ -7,14 +7,14 @@ sudo apt install -y openjdk-21-jdk maven git
 
 # Clone the application repository
 cd /home/ubuntu
-git clone https://github.com/techeazy-consulting/techeazy-devops.git
-cd techeazy-devops
+git clone https://github.com/Trainings-TechEazy/test-repo-for-devops.git
+cd test-repo-for-devops
 
 # Build the application
 mvn clean package
 
 # Run the application with root privileges so it can bind to port 80
-nohup sudo java -jar target/techeazy-devops-0.0.1-SNAPSHOT.jar > /home/ubuntu/app.out.log 2> /home/ubuntu/app.err.log &
+nohup sudo java -jar target/hellomvc-0.0.1-SNAPSHOT.jar > /home/ubuntu/app.out.log 2> /home/ubuntu/app.err.log &
 
 # Schedule automatic shutdown after 60 minutes
 echo "sudo shutdown -h now" | at now + 60 minutes
