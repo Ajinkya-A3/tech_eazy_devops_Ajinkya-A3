4a06cefee1461831ae58d2d83a71299d7cf462c0
Ajinkya-A3 <ajinkyaa3xd@gmail.com>
2025-07-26 15:39:13 +0530

    added changes for roles and policies

--- Terraform/iam_instance_profile.tf
+++ Terraform/iam_instance_profile.tf
@@ -1,6 +1,11 @@
-resource "aws_iam_instance_profile" "test" {
-  name = "webapp-profile-${var.env}"
-  role = aws_iam_role.ec2_role.name
+# Instance profile for EC2 to upload logs to S3 (Write-only access)
+resource "aws_iam_instance_profile" "upload_profile" {
+  name = "upload-profile-${var.env}"
+  role = aws_iam_role.s3_upload_only_role.name
 }
 
-
+# Instance profile for EC2 or external usage to read from S3 (Read-only access)
+resource "aws_iam_instance_profile" "readonly_profile" {
+  name = "readonly-profile-${var.env}"
+  role = aws_iam_role.s3_readonly_role.name
+}
