160975e574aa8d3dd3c332a03a51abc395f507fc
Ajinkya-A3 <ajinkyaa3xd@gmail.com>
2025-09-05 15:32:52 +0530

    modified some things according to need

--- Scripts/user_data.sh
+++ Scripts/user_data.sh
@@ -32,29 +32,29 @@ sudo nohup java -jar target/hellomvc-0.0.1-SNAPSHOT.jar \
 
 
 # Schedule log upload and shutdown
-cat <<'EOF' | at now + 3 minutes
-#!/bin/bash
+# cat <<'EOF' | at now + 3 minutes
+# #!/bin/bash
 
-# Set PATH so aws CLI and other tools are available
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+# # Set PATH so aws CLI and other tools are available
+# export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 
 
-# Ensure AWS credentials are active (via instance profile)
-aws sts get-caller-identity > /dev/null 2>&1
+# # Ensure AWS credentials are active (via instance profile)
+# aws sts get-caller-identity > /dev/null 2>&1
 
-# Make sure logs are readable
-sudo chmod -R a+r /home/ubuntu/logs
+# # Make sure logs are readable
+# sudo chmod -R a+r /home/ubuntu/logs
 
-# Copy EC2 system logs
-sudo cp /var/log/cloud-init.log /home/ubuntu/logs/ec2/
-sudo cp /var/log/cloud-init-output.log /home/ubuntu/logs/ec2/
+# # Copy EC2 system logs
+# sudo cp /var/log/cloud-init.log /home/ubuntu/logs/ec2/
+# sudo cp /var/log/cloud-init-output.log /home/ubuntu/logs/ec2/
 
-# Upload logs to S3
-aws s3 cp /home/ubuntu/logs/ec2/ s3://${s3_bucket_name}/logs/ec2/ --recursive
-aws s3 cp /home/ubuntu/logs/app/ s3://${s3_bucket_name}/logs/app/ --recursive
+# # Upload logs to S3
+# aws s3 cp /home/ubuntu/logs/ec2/ s3://${s3_bucket_name}/logs/ec2/ --recursive
+# aws s3 cp /home/ubuntu/logs/app/ s3://${s3_bucket_name}/logs/app/ --recursive
 
-# Shutdown the instance
-sudo shutdown -h now
-EOF
+# # Shutdown the instance
+# sudo shutdown -h now
+# EOF
 
